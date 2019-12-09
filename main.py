46da3ef6e12bc63f75ee7dc61f0c379cd02b35c6
epsilon91103 <47711204+epsilon91103@users.noreply.github.com>
2019-12-09 16:17:02 +0500

    search for common elements changed

--- main.py
+++ main.py
@@ -17,10 +17,8 @@ df_ext = df[['Keyword', 'AdGroupId']].assign(key=1).merge(
 
 df_ext = df_ext[df_ext.AdGroupId_x != df_ext.AdGroupId_y].copy()
 
-df_ext['crossed'] = df_ext.apply(
-    lambda x: ', '.join(set(x.Keyword_x.split()) & set(x.Keyword_y.split())),
-    axis=1
-)
+df_ext['crossed'] = [', '.join(set(x.lower().split()) & set(y.lower().split())) 
+                     for x, y in zip(df_ext.Keyword_x, df_ext.Keyword_y)]
 
 # write
 df_ext.to_csv('keywords_ext.csv', sep=';', quotechar="'")
